73a37afbeee8a5de01591bb87bc63459152d6d9f
TioStitch <guialegomes642@gmail.com>
2024-01-03 23:12:55 -0300

    Segunda Geracao

---
+++ pokemons/secondgen/totodile.lua
@@ -0,0 +1,47 @@
+local totodile = {
+    talk_name = "Totodile...",
+    nome = "Totodile",
+    vida = 16,
+    tipo = "Agua",
+
+    primeiro_ataque = "Diluvio",
+    primeiro_ataque_dano = 6,
+    segundo_ataque = "Tempestade",
+    segundo_ataque_dano = 8,
+    terceiro_ataque = "Forca Absoluta",
+    terceiro_ataque_dano = 10
+}
+
+function totodile:atacar(playerPokemon, ataque)
+    if ataque == 1 then
+        playerPokemon.vida = playerPokemon.vida - totodile.primeiro_ataque_dano
+    elseif ataque == 2 then
+        playerPokemon.vida = playerPokemon.vida - totodile.segundo_ataque_dano
+    elseif ataque == 3 then
+        playerPokemon.vida = playerPokemon.vida - totodile.terceiro_ataque_dano
+    end
+end
+
+function totodile:getAttackName(ataque)
+    if ataque == 1 then
+        return totodile.primeiro_ataque
+    elseif ataque == 2 then
+        return totodile.segundo_ataque
+    elseif ataque == 3 then
+        return totodile.terceiro_ataque
+    end
+    return ""
+end
+
+function totodile:getAttackDamage(ataque)
+    if ataque == 1 then
+        return totodile.primeiro_ataque_dano
+    elseif ataque == 2 then
+        return totodile.segundo_ataque_dano
+    elseif ataque == 3 then
+        return totodile.terceiro_ataque_dano
+    end
+    return 10
+end
+
+return totodile
